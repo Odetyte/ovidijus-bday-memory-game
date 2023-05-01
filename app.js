cede4d76c2defa480b3ddfd0a0adb8ff39870cee
Odetyte <odetamaslovaite@yahoo.com>
2023-05-01 15:25:42 +0200

    innitial js, add photos

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-console.log("hey")
+//Grab html selectors
+const section = document.querySelector("section");
+const playerLivesCount = document.querySelector("span");
+const playerLives = 6;
+
+//Link text
+playerLivesCount.textContent = playerLives;
+
+//Genereta data  for cards
+const getData = () => [
+    {imgSrc: './images/Ovidijus1.JPG'},
+    {imgSrc: './images/Ovidijus2.JPG'},
+    {imgSrc: './images/Ovidijus3.JPG'},
+    {imgSrc: './images/Ovidijus4.JPG'}
+];
+
+// randomize cards
+const randomize = () => {
+    const cardData = getData();
+    cardData.sort(() => Math.random() - 0.5);
+    return cardData;
+   
+}
+//Card generator function
+const cardGenerator = () => {
+    const cardData = randomize();
+    // Generate HTML
+    cardData.forEach((item)=> {
+        const card = document.createElement('div');
+        const face = document.createElement('img');
+        const back = document.createElement('div');
+        card.classList = 'card';
+        face.classList = 'face';
+        back.classList = 'back';
+        //Attach info to cards
+        face.src = item.imgSrc;
+        // Attach cards to the section
+        section.appendChild(card);
+        card.appendChild(face);
+        card.appendChild(back);
+       
+    })
+   
+
+
+}
+
+cardGenerator();
+// [{imgSrc: './images/Ovidijus1.JPG'}]
+
+// images.src = imgSrc
